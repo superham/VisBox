6c7a31a33bfd8a880cb2b21702bb8d93e521d8a5
Alex Kaariainen <alex.kaariainen@valpo.edu>
2017-10-18 17:42:54 -0500

    Create OnMonitor.bat

---
+++ Config/Windows/OnMonitor.bat
@@ -0,0 +1,10 @@
+#TO BE USED IN WINDOWS FOR VISBOX
+@echo off
+@color f0
+
+echo Powering on monitors
+rem Setup Device
+mode com1: baud=9600 data=8 parity=n stop=1
+rem Send bytes
+echo PWR ON > COM1
+pause
